16986ef76d2da167faed084caf86e49d8f2d6894
Thibault Falise <thibault.falise@gmail.com>
2016-05-02 17:58:14 +0200

    Add base for unit test implementation

--- SmashTheCode/Player.cs
+++ SmashTheCode/Player.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 /**
  * Auto-generated code below aims at helping you parse
@@ -10,80 +11,130 @@ namespace SmashTheCode
     {
         public static void Main(string[] args)
         {
+            var game = new Game();
 
-            // game loop
             while (true)
             {
-                var colorA = -1;
-                var colorB = -1;
-
-                for (int i = 0; i < 8; i++)
-                {
-                    string[] inputs = Console.ReadLine().Split(' ');
-
-                    if (i > 0) continue;
-
-                    colorA = int.Parse(inputs[0]); // color of the first block
-                    colorB = int.Parse(inputs[1]); // color of the attached block
-                }
-
-                Console.Error.WriteLine("Current color: " + colorA);
-
-                var columnHeights = new int[6];
-                var topColors = new int[6];
-                for (int i = 0; i < 6; i++) topColors[i] = -1;
-
-                for (int i = 0; i < 12; i++)
-                {
-                    string row = Console.ReadLine();
-
-                    for (int j = 0; j < 6; j++)
-                    {
-                        if (topColors[j] == -1 && row[j] != '.')
-                            topColors[j] = int.Parse(row[j].ToString());
-                        if (row[j] != '.')
-                            columnHeights[j]++;
-                    }
-                }
-
-                for (int i = 0; i < 12; i++)
-                {
-                    // One line of the map ('.' = empty, '0' = skull block, '1' to '5' = colored block)
-                    string row = Console.ReadLine(); 
-                }
-
-                var min = 999;
-                var minColumn = -1;
-
-                var minSameColor = 999;
-                var sameColorColumn = -1;
-
-                Console.Error.WriteLine("Top colors: " + String.Join(" ", topColors));
-
-                for (int i = 0; i < 6; i++)
-                {
-                    if (topColors[i] == colorA && columnHeights[i] < minSameColor)
-                    {
-                        minSameColor = columnHeights[i];
-                        sameColorColumn = i;
-                    }
-
-                    if (columnHeights[i] < min)
-                    {
-                        min = columnHeights[i];
-                        minColumn = i;
-                    }
-                }
-
-                Console.Error.WriteLine("Column (s): " + sameColorColumn);
-                Console.Error.WriteLine("Column: " + minColumn);
-
-                // Write an action using Console.WriteLine()
-                // To debug: Console.Error.WriteLine("Debug messages...");
-
-                // "x": the column in which to drop your blocks
-                Console.WriteLine(sameColorColumn != -1 ? sameColorColumn : minColumn); 
+                Console.WriteLine(game.ResolveTurn());
             }
+        }
+    }
+
+    public class Game
+    {
+        private readonly IConsole _console;
+
+        public Game() : this(new SystemConsole())
+        {
+            
+        }
+
+        public Game(IConsole console)
+        {
+            _console = console;
+        }
+
+        public int ResolveTurn()
+        {
+            NextColors = new GameColor[8];
+
+            for (int i = 0; i < 8; i++)
+            {
+                string[] inputs = _console.ReadLine().Split(' ');
+
+                var color = new GameColor
+                {
+                    First = int.Parse(inputs[0]),
+                    Second = int.Parse(inputs[1])
+                };
+
+                NextColors[i] = color;
+            }
+
+            _console.Debug("Current color: " + NextColors[0].First);
+
+            var columnHeights = new int[6];
+            var topColors = new int[6];
+            for (int i = 0; i < 6; i++) topColors[i] = -1;
+
+            for (int i = 0; i < 12; i++)
+            {
+                string row = _console.ReadLine();
+
+                for (int j = 0; j < 6; j++)
+                {
+                    if (topColors[j] == -1 && row[j] != '.')
+                        topColors[j] = int.Parse(row[j].ToString());
+                    if (row[j] != '.')
+                        columnHeights[j]++;
+                }
+            }
+
+            for (int i = 0; i < 12; i++)
+            {
+                // One line of the map ('.' = empty, '0' = skull block, '1' to '5' = colored block)
+                string row = _console.ReadLine();
+            }
+
+            var min = 999;
+            var minColumn = -1;
+
+            var minSameColor = 999;
+            var sameColorColumn = -1;
+
+            _console.Debug("Top colors: " + String.Join(" ", topColors));
+
+            for (int i = 0; i < 6; i++)
+            {
+                if (topColors[i] == NextColors[0].First && columnHeights[i] < minSameColor)
+                {
+                    minSameColor = columnHeights[i];
+                    sameColorColumn = i;
+                }
+
+                if (columnHeights[i] < min)
+                {
+                    min = columnHeights[i];
+                    minColumn = i;
+                }
+            }
+
+            _console.Debug("Column (s): " + sameColorColumn);
+            _console.Debug("Column: " + minColumn);
+
+            // Write an action using Console.WriteLine()
+            // To debug: Console.Error.WriteLine("Debug messages...");
+
+            // "x": the column in which to drop your blocks
+            return sameColorColumn != -1 ? sameColorColumn : minColumn;
+        }
+
+        public GameColor[] NextColors { get; private set; }
+    }
+
+    public struct GameColor
+    {
+        public int First { get; set; }
+        public int Second { get; set; }
+    }
+
+    public interface IConsole
+    {
+        string ReadLine();
+
+        void Debug(string message);
+    }
+
+    public class SystemConsole : IConsole
+    {
+        public string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+
+        public void Debug(string message)
+        {
+            Console.Error.WriteLine(message);
         }
     }
 }
---
+++ SmashTheCodeTests/GameTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NFluent;
+using NUnit.Framework;
+using SmashTheCode;
+using FakeItEasy;
+
+namespace SmashTheCodeTests
+{
+    public class GameTests
+    {
+        [Test]
+        public void Should_read_next_colors_when_resolving_a_turn()
+        {
+            var inputReader = A.Fake<IConsole>();
+            A.CallTo(() => inputReader.ReadLine()).ReturnsNextFromSequence(
+                "0 0",
+                "0 0",
+                "0 0",
+                "0 0",
+                "0 0",
+                "0 0",
+                "0 0",
+                "0 0",
+                "......",
+                "......",
+                "......",
+                "......",
+                "......",
+                "......",
+                "......",
+                "......",
+                "......",
+                "......",
+                "......",
+                "......",
+                "......",
+                "......",
+                "......",
+                "......",
+                "......",
+                "......",
+                "......",
+                "......",
+                "......",
+                "......",
+                "......",
+                "......");
+
+            var game = new Game(inputReader);
+
+            game.ResolveTurn();
+
+            Check.That(game.NextColors).ContainsExactly(
+                new GameColor { First = 0, Second = 0}, 
+                new GameColor { First = 0, Second = 0}, 
+                new GameColor { First = 0, Second = 0}, 
+                new GameColor { First = 0, Second = 0}, 
+                new GameColor { First = 0, Second = 0}, 
+                new GameColor { First = 0, Second = 0}, 
+                new GameColor { First = 0, Second = 0}, 
+                new GameColor { First = 0, Second = 0});
+        }
+    }
+
+
+}
